7c06b1e9bc6b83065da0de682074dd7bb21755ba
Junumboxo <44647094+Junumboxo@users.noreply.github.com>
2020-03-16 19:01:44 +0200

    Fixing data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ var data = [{"data": "09/03/2020", "ora": "17:00", "infectate": 1, "suspecte": 6
  {"data": "14/03/2020", "ora": "18:00", "infectate": 12, "suspecte": 25, "fise epidemologice": 2813, "monitorzare": 7106, "intrate in tara": 17218, "linia_verde": 130, "decedati": 0, "vindecati": 0},
  {"data": "15/03/2020", "ora": "10:00", "infectate": 12, "suspecte": 27, "fise epidemologice": 5651, "monitorzare": 8158, "intrate in tara": 16648, "linia_verde": 70, "decedati": 0, "vindecati": 0},
  {"data": "15/03/2020", "ora": "18:00", "infectate": 23, "suspecte": 11, "fise epidemologice": 5651, "monitorzare": 8158, "intrate in tara": 16648, "linia_verde": 70, "decedati": 0, "vindecati": 0},
- {"data": "16/03/2020", "ora": "10:00", "infectate": 23, "suspecte": 27, "fise epidemologice": 4400, "monitorzare": 9193, "intrate in tara": 14651, "linia_verde": 95, "decedati": 0, "vindecati": 0}
+ {"data": "16/03/2020", "ora": "10:00", "infectate": 23, "suspecte": 27, "fise epidemologice": 4400, "monitorzare": 9193, "intrate in tara": 14651, "linia_verde": 95, "decedati": 0, "vindecati": 0},
  {"data": "16/03/2020", "ora": "18:00", "infectate": 29, "suspecte": "-", "monitorzare": 9193, "vindecati": 1}
  ]
 var lastUpdate = "16/03/2020(18:00)";
