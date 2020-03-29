dc89955e82a3474338d2d7bc72544a6845abe660
Junumboxo <44647094+Junumboxo@users.noreply.github.com>
2020-03-29 18:21:41 +0300

    Update data.js

--- newdesign/js/data.js
+++ newdesign/js/data.js
@@ -37,10 +37,11 @@ var data = [{"ora": "17:00", "data": "09/03/2020", "vindecati": 0, "decedati": 0
 {"ora": "18:00", "data": "27/03/2020", "vindecati": 5, "decedati": 2, "monitorzare": 28510, "infectate": 199, "fise epidemologice": 1603, "intrate in tara": 1603, "suspecte": 134, "linia_verde": 215},
 {"ora": "10:00", "data": "28/03/2020", "vindecati": 5, "decedati": 2, "monitorzare": 29114, "infectate": 199, "fise epidemologice": 2340, "intrate in tara": 2340, "suspecte": 135, "linia_verde": 207}, 
 {"ora": "18:00", "data": "28/03/2020", "vindecati": 11, "decedati": 2, "monitorzare": 29114, "infectate": 231, "fise epidemologice": 2340, "intrate in tara": 2340, "suspecte": 135, "linia_verde": 207},
-{"ora": "10:00", "data": "29/03/2020", "vindecati": 11, "decedati": 2, "monitorzare": 29191, "infectate": 231, "fise epidemologice": 2361, "intrate in tara": 2361, "suspecte": 110, "linia_verde": 175}                 
+{"ora": "10:00", "data": "29/03/2020", "vindecati": 11, "decedati": 2, "monitorzare": 29191, "infectate": 231, "fise epidemologice": 2361, "intrate in tara": 2361, "suspecte": 110, "linia_verde": 175},
+{"ora": "18:00", "data": "29/03/2020", "vindecati": 13, "decedati": 2, "monitorzare": 29191, "infectate": 263, "fise epidemologice": 2361, "intrate in tara": 2361, "suspecte": 110, "linia_verde": 175}
 ]; 
-var lastUpdate = "29/03/2020(10:00)";
+var lastUpdate = "29/03/2020(18:00)";
 
 var intervalAni  = ['0-9','10-19','20-29','30-39','40-49','50-59','60-69', '70-79', '80+'];
-var infectatiAni = [7,     11,     35,     40,      39,      54,      38,      5,     2];
-var cazuriImportate = 58, infectariLocale = 173;
+var infectatiAni = [8,     12,     36,     45,      48,      61,      42,      9,     2];
+var cazuriImportate = 59, infectariLocale = 204;
