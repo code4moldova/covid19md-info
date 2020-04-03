99649fd7788b9def1edf42dcb9562a67d849fb27
Mosnoi Ion <johnsmithm@users.noreply.github.com>
2020-04-03 12:22:46 +0300

    Update data.js

--- newdesign/js/data.js
+++ newdesign/js/data.js
@@ -45,10 +45,10 @@ var data = [{"ora": "17:00", "data": "09/03/2020", "vindecati": 0, "decedati": 0
 {"ora": "18:00", "data": "31/03/2020", "vindecati": 22, "decedati": 4, "monitorzare": 26379, "infectate": 353, "fise epidemologice": 1957, "intrate in tara": 1957, "suspecte": 66, "linia_verde": 181},
 {"ora": "10:00", "data": "01/04/2020", "vindecati": 22, "decedati": 4, "monitorzare": 25529, "infectate": 353, "fise epidemologice": 1836, "intrate in tara": 1836, "suspecte": 67, "linia_verde": 181},
 {"ora": "18:00", "data": "01/04/2020", "vindecati": 22, "decedati": 4, "monitorzare": 25529, "infectate": 423, "fise epidemologice": 1836, "intrate in tara": 1836, "suspecte": 67, "linia_verde": 181},
-{"ora": "18:00", "data": "01/04/2020", "vindecati": 26, "decedati": 6, "monitorzare": 35553, "infectate": 505, "fise epidemologice": 1836, "intrate in tara": 1836, "suspecte": 192, "linia_verde": 181}
+{"ora": "18:00", "data": "02/04/2020", "vindecati": 26, "decedati": 6, "monitorzare": 35553, "infectate": 505, "fise epidemologice": 1836, "intrate in tara": 1836, "suspecte": 192, "linia_verde": 181}
 ]; 
-var lastUpdate = "01/04/2020(18:00)";
+var lastUpdate = "02/04/2020(18:00)";
 
 var intervalAni  = ['0-9','10-19','20-29','30-39','40-49','50-59','60-69', '70-79', '80+'];
 var infectatiAni = [14,     28,     50,     79,      65,      101,      63,      19,     2];
-var cazuriImportate = 74, infectariLocale = 347;
+var cazuriImportate = 79, infectariLocale = 426;
