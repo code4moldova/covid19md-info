1195257062c3ba3ab291052655929d476a3cbe76
Tatiana Barbova <44647094+Junumboxo@users.noreply.github.com>
2020-04-13 11:40:55 +0300

    Update data.js

--- newdesign/js/data.js
+++ newdesign/js/data.js
@@ -62,10 +62,11 @@ var data = [{"ora": "17:00", "data": "09/03/2020", "vindecati": 0, "decedati": 0
 {"ora": "18:00", "data": "10/04/2020", "vindecati": 56, "decedati": 29, "monitorzare": 38459, "infectate": 1438, "fise epidemologice": 1600, "intrate in tara": 1600, "suspecte": 246, "linia_verde": 181},
 {"ora": "10:00", "data": "11/04/2020", "vindecati": 75, "decedati": 30, "monitorzare": 39131, "infectate": 1438, "fise epidemologice": 1396, "intrate in tara": 1396, "suspecte": 129, "linia_verde": 181},
 {"ora": "18:00", "data": "11/04/2020", "vindecati": 75, "decedati": 30, "monitorzare": 39131, "infectate": 1560, "fise epidemologice": 1396, "intrate in tara": 1396, "suspecte": 129, "linia_verde": 181},
-{"ora": "10:00", "data": "12/04/2020", "vindecati": 94, "decedati": 30, "monitorzare": 39180, "infectate": 1560, "fise epidemologice": 1396, "intrate in tara": 1396, "suspecte": 96, "linia_verde": 181},
-{"ora": "18:00", "data": "12/04/2020", "vindecati": 94, "decedati": 31, "monitorzare": 39180, "infectate": 1662, "fise epidemologice": 1396, "intrate in tara": 1396, "suspecte": 96, "linia_verde": 181}            
+{"ora": "10:00", "data": "12/04/2020", "vindecati": 94, "decedati": 30, "monitorzare": 39180, "infectate": 1560, "fise epidemologice": 835, "intrate in tara": 835, "suspecte": 96, "linia_verde": 181},
+{"ora": "18:00", "data": "12/04/2020", "vindecati": 94, "decedati": 31, "monitorzare": 39180, "infectate": 1662, "fise epidemologice": 835, "intrate in tara": 835, "suspecte": 96, "linia_verde": 181},
+{"ora": "10:00", "data": "13/04/2020", "vindecati": 107, "decedati": 33, "monitorzare": 39314, "infectate": 1662, "fise epidemologice": 1395, "intrate in tara": 1395, "suspecte": 74, "linia_verde": 181}            
 ]; 
-var lastUpdate = "12/04/2020(18:00)";
+var lastUpdate = "13/04/2020(10:00)";
 
 var intervalAni  = ['0-9','10-19','20-29','30-39','40-49','50-59','60-69', '70-79', '80+'];
 var infectatiAni = [58,     67,     185,     256,      289,      398,      288,      102,     20];
